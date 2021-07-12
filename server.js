12e674f944c60f1c217ff28fa4e1e6f06eed670e
swissboy76 <85522991+swissboy76@users.noreply.github.com>
2021-07-13 00:08:09 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var http = require("http");
+setting_detail = {};
+
+
+        //mongoose = require('./config/mongoose'),
+        express = require('./config/express'),
+        //db = mongoose(),
+        app = express();
+const port = '7001';
+app.listen(port);
+
+
+    console.log('Magic happens on port ' + port); 
+
+exports = module.exports = app;
